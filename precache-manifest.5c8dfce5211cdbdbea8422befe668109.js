2c37986b79fb96ae2d412dfeaeb5614a5081d6c8
Emanuelpna <emanuelpna@yahoo.com.br>
2020-01-06 00:43:15 -0300

    Criação da página

---
+++ precache-manifest.5c8dfce5211cdbdbea8422befe668109.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9258c3ccd708a77d8bdc52e456a6067b",
+    "url": "/ecomm/index.html"
+  },
+  {
+    "revision": "255147b62424d5168149",
+    "url": "/ecomm/static/css/main.d84374d7.chunk.css"
+  },
+  {
+    "revision": "f661afc5cc511862a7f4",
+    "url": "/ecomm/static/js/2.45a83f52.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/ecomm/static/js/2.45a83f52.chunk.js.LICENSE"
+  },
+  {
+    "revision": "255147b62424d5168149",
+    "url": "/ecomm/static/js/main.24a5260f.chunk.js"
+  },
+  {
+    "revision": "7694c2e1143e0253b210",
+    "url": "/ecomm/static/js/runtime-main.49fb06c4.js"
+  },
+  {
+    "revision": "8fa01d33394b4d8e1dc4a225436cef2f",
+    "url": "/ecomm/static/media/banner.8fa01d33.jpg"
+  },
+  {
+    "revision": "08ff718acdb765127aa01340ee9c5939",
+    "url": "/ecomm/static/media/banner2.08ff718a.jpg"
+  }
+]);
